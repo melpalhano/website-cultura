cc52e800b2688d5ab620c8a26e499ca0f70b149e
Melissa Palhano <46391712+melpalhano@users.noreply.github.com>
2021-09-29 10:23:44 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-let navbar = document.querySelector('.navbar'); //Retorna o primeiro elemento dentro do documento
-    document.querySelector('#pontos-turisticos-sm').onclick = () => {
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-
-}
-
